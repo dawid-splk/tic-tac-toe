dca71f3febc0967c3187b8b6a73cc2bb32353fe4
dawid splk <dawid.t.spalek@gmail.com>
2024-05-15 20:46:07 +0200

    add readme, some comments and remove amplify from package.json

---
+++ README.md
@@ -0,0 +1,8 @@
+leaving tips for starting app:
+
+1. Clone the repository
+2. Update user pool ID and region in src/main/resources/application.properties 
+3. Update user pool ID and client ID in frontend/src/UserPool.js
+4. Run `npm install` insinde frontend folder
+5. Run `npm start` inside frontend folder
+6. Start backend spring app in your IDE
--- src/main/java/com/cloudprogramming/config/SecurityConfig.java
+++ src/main/java/com/cloudprogramming/config/SecurityConfig.java
@@ -11,13 +11,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final AuthenticationService authenticationService;
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,12 +26,14 @@ public class SecurityConfig {
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/game/hello").permitAll()
-                .requestMatchers("/gameplay/**").permitAll()
-                .anyRequest().authenticated()
+                .requestMatchers("/gameplay/**").permitAll()      // public because of cors issue, acceptable solution coz websocket
+                .anyRequest().authenticated()                       // requires app and topic prefix (like a token) and is after login anyway
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(authenticationService),
                 UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(new CorsFilter(), UsernamePasswordAuthenticationFilter.class);;
+                .addFilterAfter(new CorsFilter(), UsernamePasswordAuthenticationFilter.class);
+                // pretty sure that addFilterAfter and CorsFilter are not needed, but once it's working then it's not
+                // leaving it for now
 
         return http.build();
     }
